794eb01068412cfd9c4c3d783460fbaaa5c19b35
sdetai <aigulidrisova8@gmail.com>
2023-11-10 12:39:29 -0500

    timer functionality addded

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 const start=document.querySelector('.start')
 const headerText=document.querySelector('h1')
 const main=document.querySelector('main')
-const boxes=document.querySelectorAll()   
+const boxes=document.querySelectorAll('.box')   
+const aside2=document.querySelector('.aside2')
+const random1=document.querySelector('.random1')
+const random2=document.querySelector('.random2')
+const timerEl=document.getElementById('timer')
 
+const color1="rgb(42, 158, 158)";
+const color2="rgb(190, 57, 57)";
 
+let mainState=true;
+let playButtonClicked=false;
 
 
  footer.addEventListener('click', function(evt){
@@ -14,10 +22,52 @@ const boxes=document.querySelectorAll()
     headerText.innerText=`Player #${player} starts! Click play button!`
 console.log(headerText)
  }else if(evt.target.classList.contains('reset')){
-main.innerHTML='';
+console.dir(boxes)
+ // there should be a code to clear from color
  }
  })
+
+aside2.addEventListener('click',function(evt){
+    if(evt.target.classList.contains('play')){
+        if(!playButtonClicked){
+     random1.innerText=randomIntFromInterval(1,4);
+     random2.innerText=randomIntFromInterval(1,4);
+     random1.classList.add('clicked')
+     random2.classList.add('clicked')
+     playButtonClicked=true;
+     timerCountDown(timerEl);
+    }
+}
+})
+
+function timerCountDown(timerElement){
+   let count=extractNumericValue(timerElement);
+    const setTimer=setInterval(function(){
+        if(count<=0){
+            clearInterval(setTimer);
+            timerElement.innerText='Finished';   
+            playButtonClicked = false;
+      random1.classList.remove('clicked');
+      random2.classList.remove('clicked');
+        }else{
+            timerElement.innerText=count+ " seconds"
+        }
+        count--;
+    },1000);
+}
+
+main.addEventListener('mouseover',function(evt){
+    
+})
+
+function extractNumericValue(element){
+    let textContent=element.innerText || element.textContent;
+    let numericPart=textContent.match(/\d+/)
+    let numericValue=parseInt(numericPart,10);
+    return  numericValue;
+      }
 
  function randomIntFromInterval(min, max) { 
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
+
